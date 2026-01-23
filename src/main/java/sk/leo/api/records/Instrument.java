e873b67915054eb6d7b68bda5496f4e6b3668d57
leohe <leo.heriban@gmail.com>
2026-01-23 16:56:44 +0100

    Extended information

--- src/main/java/sk/leo/api/records/Instrument.java
+++ src/main/java/sk/leo/api/records/Instrument.java
@@ -6,7 +6,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record Instrument(
         String ticker,
         String type, //Stock, ETF or ...
+        int workingScheduleId,
+        String isin,
         String currencyCode,
         String name,
+        double maxOpenQuantity,
         Boolean extendedHours
 ) {}
