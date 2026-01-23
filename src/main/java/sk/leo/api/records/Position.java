d1baeb886ea28a199385d25c08be11be2551abf9
leohe <leo.heriban@gmail.com>
2026-01-23 17:48:42 +0100

    Extended information

--- src/main/java/sk/leo/api/records/Position.java
+++ src/main/java/sk/leo/api/records/Position.java
@@ -3,28 +3,55 @@ package sk.leo.api.records;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Position (
+public record Position(
         BaseCredentials instrument,
         double quantity,
-        Double averagePrice,
         Double currentPrice,
-        Double unrealizedPnl
+        WalletImpact walletImpact
 ) {
-    public String ticker(){
-        return instrument != null ? instrument.ticker() : null;
+    public String ticker() {
+        return instrument.ticker();
     }
 
-    public String name(){
-        return instrument != null ? instrument.name() : null;
+    public String name() {
+        return instrument.name();
     }
 
-    public double currentValue(){
-        return currentPrice != null ? quantity * currentPrice : 0.0;
+    public String isin() {
+        return instrument.isin();
+    }
+
+    public String currency() {
+        return instrument.currency();
+    }
+
+    public Double totalCost(){
+        return walletImpact().totalCost();
+    }
+
+    public Double currentValue(){
+        return walletImpact().currentValue();
+    }
+
+    public Double unrealisedPL(){
+        return walletImpact.unrealizedProfitLoss;
+    }
+
+    public double appreciation(){
+        return unrealisedPL() / totalCost();
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public record BaseCredentials(
             String ticker,
-            String name
+            String name,
+            String isin,
+            String currency
+    ) { }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record WalletImpact(
+        Double totalCost,
+        Double currentValue,
+        Double unrealizedProfitLoss
     ) {}
 }
